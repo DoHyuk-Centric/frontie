482caff819aea078d6719c7c75e4e74a51485f4e
DoHyuk-Centric <clzlsdlwhgdk12@gmail.com>
2026-05-17 14:58:13 +0900

    feat: LogoIcon 제작

--- app/Chat/page.tsx
+++ app/Chat/page.tsx
@@ -1,14 +1,21 @@
 import { Button } from "@/components/ui/button";
+import LogoIcon from "@/components/icons/LogoIcon";
 
-export default function Chat(){
-    return(
-        <main>
-            <h2>프론트에게 물어보세요.</h2>
-            <p>무엇을 도와드릴까요? 아래 제안 중 하나를 선택하거나 직접 질문해보세요.</p>
+export default function Chat() {
+  return (
+    <main className="flex flex-col items-center">
+      <LogoIcon />
+      <h2 className="text-[24px]">프론트에게 물어보세요.</h2>
+      <p className="text-[14px] text-[#717182] text-center">
+        무엇을 도와드릴까요? 아래 제안 중 하나를 선택하거나 직접 질문해보세요.
+      </p>
 
-            <div>
-                <Button>코드 작성 도움</Button>
-            </div>
-        </main>
-    )
+      <div className="flex flex-col w-full">
+        <Button>코드 작성 도움</Button>
+        <Button>코드 작성 도움</Button>
+        <Button>코드 작성 도움</Button>
+        <Button>코드 작성 도움</Button>
+      </div>
+    </main>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header/page";
+import Chat from "@/app/Chat/page";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Chat />
     </>
   );
 }
